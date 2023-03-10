8485297f6a4ba2777459ed96d0280871ee3c8717
Shessire <wisuwatpond01@gmail.com>
2023-03-10 14:35:20 +0700

    type of data

--- script.js
+++ script.js
@@ -3,3 +3,36 @@ function Hello() {
     console.log("The button has been clicked");
     document.getElementById("button").innerHTML = "buttton clicked";
 }
+
+console.log('Hello World!')
+
+let email = 'www@gmail.com'
+console.log(email);
+
+let bidmas = 5*(10-3)**2;
+console.log(bidmas);
+
+let selectedFruits = ['apple', 'banana', 'orange'];
+selectedFruits [3] = 'Pineapple';
+selectedFruits [1] = 'Roseapple'
+console.log(selectedFruits[3]);
+console.log(selectedFruits.length);
+
+let user = {
+        firstName: 'P',
+        email: 'p@gmail.com',
+        age: 26,
+        location: 'TH',
+        readingList: ['HTML', 'CSS', 'JS'],
+}
+
+console.log(user);
+console.log(user.email)
+user.age = 50;
+console.log(readingList[2]);
+console.log(readingList.length-1); //choose the last one of array
+
+let x = 123;
+x = 'me';
+console.log(x);
+console.log(typeof x);
