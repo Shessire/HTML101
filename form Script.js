fe3c984216ba29f06e797e45b1d695787d250ffa
Shessire <wisuwatpond01@gmail.com>
2023-03-22 15:01:23 +0700

    extends class

--- form Script.js
+++ form Script.js
@@ -7,3 +7,41 @@
 //     list.textContent = 'book title: ' + list.textContent;
 // })
 
+
+class User {
+    constructor (name, birthdate, gender) {
+        this._name = name;
+        this._birthdate = birthdate;
+        this._gender = gender;
+        this._logIn = 0;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get birthdate() {
+        return this._birthdate;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    logIn () {
+        this._logIn++;
+    }
+}
+
+class PrivUser extends User {
+    constructor (name, birthdate, gender, access) {
+        super (name, birthdate, gender);
+        this._access = access;
+    }
+
+    get access () {
+        return this._access = access;
+    }
+}
+let User1 = new User ('Pond', '2/2/22', 'male');
+let User2 = new PrivUser('Pondd', '2/3/22', 'male', true);
