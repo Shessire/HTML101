81a502d47b039f3d22e012db4fd7851a0b5329a0
Shessire <wisuwatpond01@gmail.com>
2023-03-22 14:04:35 +0700

    gJava

---
+++ form Script.js
@@ -0,0 +1,9 @@
+// const gJavascript = document.querySelector('#reading-list li:nth-child(2) .name');
+// console.log(gJavascript);
+// gJavascript.innerHTML = 'advgbvwrbew';
+
+// const readingList = document.querySelectorAll('#reading-list li .name');
+// readingList.forEach(function(list) {
+//     list.textContent = 'book title: ' + list.textContent;
+// })
+
